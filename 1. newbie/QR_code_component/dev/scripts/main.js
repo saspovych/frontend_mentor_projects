ba855a8985dc0cccc4434e06b92452fa36480b7f
Alexandr <90194375+saspovych@users.noreply.github.com>
2022-01-26 17:31:48 +0300

    Delete 1. newbie directory

--- 1. newbie/QR_code_component/dev/scripts/main.js
+++
@@ -1,8 +0,0 @@
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-	text: "https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H",
-	width: 200,
-	height: 200,
-	colorDark : "#FFFFFF",
-	colorLight : "#0B7FAB",
-	correctLevel : QRCode.CorrectLevel.H
-});
